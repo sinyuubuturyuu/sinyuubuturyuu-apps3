f9407e561a3d2d2a2691355bff74ca19008eb213
yuu <yuuiti.htnk167@gmail.com>
2026-04-23 21:06:15 +0900

    Refresh driver points cache version

--- sinyuubuturyuu/getujitiretenkenhyou/sw.js
+++ sinyuubuturyuu/getujitiretenkenhyou/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "monthly-tire-check-v49";
+const CACHE_NAME = "monthly-tire-check-v50";
 const ASSETS = [
   "./",
   "./index.html",
--- sinyuubuturyuu/sw.js
+++ sinyuubuturyuu/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "sinyuubuturyuu-launcher-v23";
+const CACHE_NAME = "sinyuubuturyuu-launcher-v24";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./launcher.css",
   "./launcher.js",
   "./shared-settings.js?v=20260405a",
-  "./driver-points/driver-points.js?v=20260404d",
+  "./driver-points/driver-points.js?v=20260423a",
   "./manifest.webmanifest",
   "./sinyuubuturyuu-icon.png",
   "./apple-touch-icon.png",
